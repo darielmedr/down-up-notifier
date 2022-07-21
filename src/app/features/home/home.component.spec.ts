3548f14b48bcf71189f5d88d1aa409dd59f099b4
darielmedr <darielmedr97@gmail.com>
2022-07-21 21:57:06 +0200

    test(homecomponent): setup test suite

--- src/app/features/home/home.component.spec.ts
+++ src/app/features/home/home.component.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponents } from 'ng-mocks';
+import { AutoTypingComponent } from './components/auto-typing/auto-typing.component';
+import { PhoneTemplateComponent } from './components/phone-template/phone-template.component';
+import { SmsNotificationComponent } from './components/sms-notification/sms-notification.component';
 
 import { HomeComponent } from './home.component';
 
-describe('HomeComponent', () => {
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        HomeComponent,
+        ...MockComponents(
+          AutoTypingComponent,
+          PhoneTemplateComponent,
+          SmsNotificationComponent,
+        ),
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
